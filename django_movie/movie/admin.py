344e7cdb36690ab1416c2de2f2c10eb7d728f12b
Fenominall <vladislav.todorov333@gmail.com>
2021-03-28 22:02:21 +0300

    admin dashboard customizing

--- django_movie/movie/admin.py
+++ django_movie/movie/admin.py
@@ -2,11 +2,36 @@ from django.contrib import admin
 from .models import Category, Movie, MovieShots, Actor, Rating, RatingStar, Reviews, Genre
 
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Movie)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "url")
+    list_display_links = ("name", )
+
+
+class RevieInlines(admin.TabularInline):
+    model = Reviews
+    extra = 1
+    readonly_fields = ("name", "email")
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "url", "draft")
+    list_filter = ("category", "year")
+    search_fields = ("title", "category__name") # __ for identofying which atribut will be taken
+    inlines = [RevieInlines]
+    save_on_top = True
+
+
+@admin.register(Reviews)
+class ReviewsAdmin(admin.ModelAdmin):
+    list_display = ("name", "email", "parent", "movie", "id")
+    readonly_fields = ("name", "email")
+
+
 admin.site.register(MovieShots)
 admin.site.register(Actor)
 admin.site.register(Rating)
 admin.site.register(RatingStar)
-admin.site.register(Reviews)
 admin.site.register(Genre)
